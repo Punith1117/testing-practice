238ad739f7be97f26e3016b9a9c7705711a45c46
Punith1117 <punith11172006@gmail.com>
2025-01-11 16:24:05 +0530

    add test to expect same string for string with non-alphabetic first character

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -18,4 +18,8 @@ describe('capitalize function', () => {
   it('should return null if a non-string value is passed', () => {
     expect(capitalizeFirstLetter(123)).toBeNull();
   })
+
+  it('should return the same string if the first letter in string is not an alphabet', () => {
+    expect(capitalizeFirstLetter('-hello')).toBe('-hello');
+  })
 });
