7b1dc1bb90a91e492692edfd9ea5089b62655414
Punith1117 <punith11172006@gmail.com>
2025-01-11 14:52:13 +0530

    see if correct outputs are returned for different inputs

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -6,4 +6,8 @@ describe('capitalize function', () => {
   it('should capitalize only first letter of a string', () => {
     expect(capitalizeFirstLetter('all')).toBe('All');
   });
+
+  it('should return correct value for each different inputs', () => {
+    expect(capitalizeFirstLetter('hello')).toBe('Hello');
+  });
 });
