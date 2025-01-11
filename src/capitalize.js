4d329610d0adb7bc776a4da3debfebd0f761e78a
Punith1117 <punith11172006@gmail.com>
2025-01-11 16:04:51 +0530

    return null if a non-string is passed

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,5 +1,6 @@
 export let capitalizeFirstLetter = (str) => {
     if (str === undefined) return null;
+    if (typeof str !== 'string') return null;
 
     let firstLetter = str.charAt(0);
     let res = str.replace(firstLetter, firstLetter.toUpperCase())
