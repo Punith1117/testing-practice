59309b3edca62bf6d4b0326af756f2581d7a76b4
Punith1117 <punith11172006@gmail.com>
2025-01-11 16:55:33 +0530

    add test that expects null if no parameter is passed

--- src/reverse-string.test.js
+++ src/reverse-string.test.js
@@ -8,4 +8,8 @@ describe('reverse string function', () => {
     it('should return a reversed string for different inputs', () => {
         expect(reverseString('hello')).toBe('olleh');
     });
+
+    it('should return null if no parameter is passed', () => {
+        expect(reverseString()).toBeNull();
+    });
 })
