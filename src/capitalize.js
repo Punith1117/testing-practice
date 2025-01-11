dfb04212590c000c99440a3e89a970d259c26b93
Punith1117 <punith11172006@gmail.com>
2025-01-11 15:16:50 +0530

    return null if no parameter is passed

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,6 @@
 export let capitalizeFirstLetter = (str) => {
+    if (str === undefined) return null;
+
     let firstLetter = str.charAt(0);
     let res = str.replace(firstLetter, firstLetter.toUpperCase())
     return res;
