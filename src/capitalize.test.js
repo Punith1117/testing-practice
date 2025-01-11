386ccfb78d0cc424d21f6b4dc81fcf0572393d52
Punith1117 <punith11172006@gmail.com>
2025-01-11 16:30:08 +0530

    add test to check if function handles more than 1 parameter correctly
    the function should only transform the first parameter if more than one parameter is passed

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -22,4 +22,8 @@ describe('capitalize function', () => {
   it('should return the same string if the first letter in string is not an alphabet', () => {
     expect(capitalizeFirstLetter('-hello')).toBe('-hello');
   })
+
+  it('should check only first parameter if more than one parameter is passed', () => {
+    expect(capitalizeFirstLetter('hello', 'world')).toBe('Hello');
+  });
 });
