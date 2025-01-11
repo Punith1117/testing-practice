a03de6b35d3dc21038615a01f6eeae0fc021bd31
Punith1117 <punith11172006@gmail.com>
2025-01-11 16:45:14 +0530

    add test to check if function returns correct values for different inputs

--- src/reverse-string.test.js
+++ src/reverse-string.test.js
@@ -4,4 +4,8 @@ describe('reverse string function', () => {
     it('should return a reversed string', () => {
         expect(reverseString('abc')).toBe('cba');
     });
+
+    it('should return a reversed string for different inputs', () => {
+        expect(reverseString('hello')).toBe('olleh');
+    });
 })
