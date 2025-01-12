fe2c680f435864f16e66739b65ff348f36d5e6bf
Punith1117 <punith11172006@gmail.com>
2025-01-12 14:12:41 +0530

    add test that expects correct outputs for different inputs

--- src/caesar-cipher.test.js
+++ src/caesar-cipher.test.js
@@ -4,4 +4,8 @@ describe('Caesar-Cipher function', () => {
     it('should return ciphered text', () => {
         expect(caesarCipher('xyz', 3)).toBe('abc')
     })
+
+    it('should return correct outputs for different inputs', () => {
+        expect(caesarCipher('abc', 3)).toBe('def')
+    })
 })
