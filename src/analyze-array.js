80a0b2476b0112446eb90f780c623920b35e6a7c
Punith1117 <punith11172006@gmail.com>
2025-01-12 16:18:40 +0530

    modify function to search for maximum element

--- src/analyze-array.js
+++ src/analyze-array.js
@@ -1,16 +1,22 @@
 export let analyzeArray = (arr) => {
     let obj = {}
-    let min
+    let min, max
 
     for (let i = 0; i < arr.length; i++) {
         if (i == 0) {
             min = arr[i]
+            max = arr[i]
             continue
         }
+
         if (arr[i] < min)
             min = arr[i]
+
+        if (arr[i] > max)
+            max = arr[i]
     }
-    
+
     obj.min = min
+    obj.max = max
     return obj
 }
