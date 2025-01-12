dd763cf795bd33c4227711825ccad1e63f017017
Punith1117 <punith11172006@gmail.com>
2025-01-12 14:51:52 +0530

    make the function un-alter punctuations

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,14 +1,26 @@
 export let caesarCipher = (str, num) => {
     let strArr = str.split('');
     let resArr = []
-    let charNum, resCharNum, charNumOfz, charNumOfZ, charNumOfa
+    let charNum, resCharNum, charNumOfz, charNumOfZ, charNumOfa, charNumOfA
     
     for (let character of strArr) {
         charNum = character.charCodeAt(0);
         resCharNum = charNum + num
         charNumOfa = 'a'.charCodeAt(0);
+        charNumOfA = 'A'.charCodeAt(0);
         charNumOfz = 'z'.charCodeAt(0);
         charNumOfZ = 'Z'.charCodeAt(0);
+
+        if (charNum < charNumOfA) {
+            resArr.push(character)
+            continue
+        } else if (charNum > charNumOfZ && charNum < charNumOfa) {
+            resArr.push(character)
+            continue
+        } else if (charNum > charNumOfz) {
+            resArr.push(character)
+            continue
+        }
 
         if (resCharNum > charNumOfz) {
             resCharNum -= 26
@@ -18,6 +30,6 @@ export let caesarCipher = (str, num) => {
         let char = String.fromCharCode(resCharNum)
         resArr.push(char);
     }
-    
+
     return resArr.join('')
 }
