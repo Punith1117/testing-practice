54fa0e28c266627bc34c2e65617b8759bea6c011
Punith1117 <punith11172006@gmail.com>
2025-01-12 13:38:02 +0530

    add test that expects result rounded to two decimal places
    division function should return result rounded to two decimal places when the result contains more than two decimal places

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -13,12 +13,16 @@ describe('calculator object', () => {
     it('should divide two numbers', () => {
         expect(calc.divide(8, 4)).toBe(2);
     })
-    
+
     it('should multiply two numbers', () => {
         expect(calc.multiply(3, 4)).toBe(12);
     })
 
     it('should return null if a number is divided by 0', () => {
         expect(calc.divide(3, 0)).toBeNull();
+    })
+
+    it('should return result rounded off to two decimal places when parameters are not integers', () => {
+        expect(calc.divide(5, 3)).toBe(1.67);
     })
 })
