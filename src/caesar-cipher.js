1470b6c1cbc567f67a4dd545b70f65fe9267d46a
Punith1117 <punith11172006@gmail.com>
2025-01-12 14:41:04 +0530

    modify the function to return correct values for different inputs

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,3 +1,23 @@
 export let caesarCipher = (str, num) => {
-    return 'abc'
+    let strArr = str.split('');
+    let resArr = []
+    let charNum, resCharNum, charNumOfz, charNumOfZ, charNumOfa
+    
+    for (let character of strArr) {
+        charNum = character.charCodeAt(0);
+        resCharNum = charNum + num
+        charNumOfa = 'a'.charCodeAt(0);
+        charNumOfz = 'z'.charCodeAt(0);
+        charNumOfZ = 'Z'.charCodeAt(0);
+
+        if (resCharNum > charNumOfz) {
+            resCharNum -= 26
+        } else if (resCharNum > charNumOfZ && charNum < charNumOfa) {
+            resCharNum -= 26
+        }
+        let char = String.fromCharCode(resCharNum)
+        resArr.push(char);
+    }
+    
+    return resArr.join('')
 }
